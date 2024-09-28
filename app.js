0cd3895f4209f67ebc5e3e19ce1db204d137e887
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-28 07:44:51 +0100

    Updated the app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const HOSTNAME = '127.0.0.1';
@@ -8,6 +9,15 @@ const PORT = process.env.PORT || 4000;
 const DB_URL = process.env.DB_URL;
 
 const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+const doctorsRouter = require('./routes/doctorsRoute');
+
+
+
+app.use('api/doctors', doctorsRouter);
 
 mongoose.connect(DB_URL).then(() => {
   console.log(`Mongodb Server Started`);
