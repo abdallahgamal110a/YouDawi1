70e573827cb49b621dc989afea7b36b2a18b3fc2
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2024-09-28 03:44:25 +0300

    Feat: More components, components preview page and route buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './components/Login';
 import './App.css';
-// import Navbar from './components/Navbar';
 import DoctorList from './components/DoctorList';
 import About from './components/About';
 import Layout from './components/Layout';
@@ -11,32 +10,34 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Appointements from './components/Appointements';
 import Patients from './components/Patients';
+import ComponentsPreview from './components/ComponentsPreview';
 
 
 function App() {
-  const isLoggedIn = false; // Example for checking login status
+  // const isLoggedIn = false; // Example for checking login status
 
   return (
     <Router>
       <Routes>
-        {/* Render the Layout only if user is logged in */}
-        {isLoggedIn ? (
-          <Route path="/" element={<Layout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/doctors" element={<DoctorList />} />
-            <Route path="/appointments" element={<Appointements />} />
-            <Route path="/patients" element={<Patients />} />
-          </Route>
-        ) : (
-          // Different component for non-logged-in users (landing page)
-          <Route>
-            {/* <Navbar /> */}
-            <Route path="/home" element={<LandingPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
-        )}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/components" />} /> {/* Redirect root to /home */}
+          <Route path="home" element={<Home />} />
+          <Route path="doctors" element={<DoctorList />} />
+          <Route path="appointments" element={<Appointements />} />
+          <Route path="patients" element={<Patients />} />
+
+        </Route>
+        <Route>
+          <Route path="/" element={<Navigate to="/components" />} /> {/* Redirect root to /home */}
+          <Route path="home" element={<LandingPage />} />
+          <Route path="about" element={<About />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="components" element={<ComponentsPreview />} />
+
+        </Route>
+        {/* Catch-all route for unmatched paths */}
+        {/* <Route path="*" element={<Navigate to={isLoggedIn ? "/home" : "/login"} />} /> */}
       </Routes>
     </Router>
   );
