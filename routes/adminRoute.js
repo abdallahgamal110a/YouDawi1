4d2c09ef34143e6e209c9c0af9028c41fcb60551
Y-admen <yassminash305@gmail.com>
2024-10-10 21:54:25 +0300

    admin CRUD operations for nurse

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const adminController = require('../controllers/adminController');
 const doctorsController = require('../controllers/doctorsController');
+const nursesController = require('../controllers/nursesController');
 const verifyToken = require('../middlewares/verifyToken');
 const allowedTo = require('../middlewares/allowedTo');
 const userRoles = require('../utils/userRoles');
@@ -18,5 +19,14 @@ router.route('/doctors/:id')
     .get(verifyToken, allowedTo(userRoles.ADMIN), doctorsController.getDoctorById)
     .put(verifyToken, allowedTo(userRoles.ADMIN), upload.single('avatar'), doctorsController.updateDoctor)
     .delete(verifyToken, allowedTo(userRoles.ADMIN), doctorsController.deleteDoctor);
+
+router.route('/nurses')
+    .get(verifyToken, allowedTo(userRoles.ADMIN), nursesController.getAllNurses)
+    .post(upload.single('avatar'), allowedTo(userRoles.ADMIN), doctorsController.registerNurse);
+
+router.route('/nurses/:id')
+    .get(verifyToken, allowedTo(userRoles.ADMIN), nursesController.getNurseById)
+    .put(verifyToken, allowedTo(userRoles.ADMIN), upload.single('avatar'), nursesController.updateNurse)
+    .delete(verifyToken, allowedTo(userRoles.ADMIN), nursesController.deleteNurse);
 
 module.exports = router;
