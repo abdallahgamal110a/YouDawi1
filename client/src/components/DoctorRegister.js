ed54bb800bce9a8ab587a5743303702612b38e5f
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2024-10-13 01:57:40 +0300

    Refactor: fix error

--- client/src/components/DoctorRegister.js
+++ client/src/components/DoctorRegister.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { registerDoctor } from '../services/doctorService';
+import { registerDoctor } from './services/doctorService';
 
 const DoctorRegisterForm = () => {
   const [formData, setFormData] = useState({
