93d7af274b35ba25fe6ad15790edd64c7f63e3ad
Abdallah Gamal Hassan <119704743+abdallahgamal110a@users.noreply.github.com>
2024-10-18 14:57:59 +0300

    Update patientRoute.js

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -6,16 +6,27 @@ const allowedTo = require('../middlewares/allowedTo')
 const userRoles = require('../utils/userRoles')
 
 router.route('/register')
-    .post(patientController.registerPatient);
+  .post(upload.single('avatar'), patientController.registerPatient);
 
 router.route('/login')
-    .post(patientController.login);
+  .post(patientController.login);
+
+router.route('/requestResetPassword')
+  .post(patientController.requestResetPassword);
+
+router.route('/resetPassword/:token')
+  .post(patientController.resetPassword);
 
 router.route('/profile')
-    .get(verifyToken, allowedTo(userRoles.ADMIN, userRoles.PATIENT), patientController.getProfile);
+  .get(verifyToken, allowedTo(userRoles.ADMIN, userRoles.PATIENT), patientController.getProfile);
 
 router.route('/')
-    .get(verifyToken, patientController.getAllPatients);
+  .get(verifyToken, patientController.getAllPatients);
+
+router.route('/:id')
+  .get(verifyToken, patientController.getPatientById)
+  .put(verifyToken, allowedTo(userRoles.DOCTOR, userRoles.ADMIN), patientController.updatePatient)
+  .delete(verifyToken, allowedTo(userRoles.DOCTOR, userRoles.ADMIN), patientController.deletePatient);
 
 
 module.exports = router;
