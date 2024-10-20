cb1ac830bb85a87b683e8ff8fb2e33f3bb84b76f
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2024-10-20 20:37:42 +0300

    Refactor: temp prevent protected route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,34 +2,34 @@ import { Navigate } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const token = localStorage.getItem('token');
+  // const token = localStorage.getItem('token');
 
-  if (!token) {
-    return <Navigate to="/login" />; // Redirect to login if no token is found
-  }
+  // if (!token) {
+  //   return <Navigate to="/login" />; // Redirect to login if no token is found
+  // }
 
-  try {
-    const user = jwtDecode(token); // Decode JWT to get user info
+  // try {
+  //   const user = jwtDecode(token); // Decode JWT to get user info
 
-    // Check if token is expired
-    const currentTime = Date.now() / 1000; // Get current time in seconds
-    if (user.exp < currentTime) {
-      localStorage.removeItem('token'); // Remove expired token
-      return <Navigate to="/login" />; // Redirect to login if expired
-    }
+  //   // Check if token is expired
+  //   const currentTime = Date.now() / 1000; // Get current time in seconds
+  //   if (user.exp < currentTime) {
+  //     localStorage.removeItem('token'); // Remove expired token
+  //     return <Navigate to="/login" />; // Redirect to login if expired
+  //   }
 
-    // Check if the user role is allowed
-    if (!allowedRoles.includes(user.role)) {
-      return <Navigate to="/unauthorized" />; // Redirect to unauthorized page
-    }
+  //   // Check if the user role is allowed
+  //   if (!allowedRoles.includes(user.role)) {
+  //     return <Navigate to="/unauthorized" />; // Redirect to unauthorized page
+  //   }
 
     return children; // Render the child components if all checks pass
 
-  } catch (error) {
-    // Clear invalid or corrupt token
-    localStorage.removeItem('token');
-    return <Navigate to="/login" />; // Redirect to login if token is invalid
-  }
+  // } catch (error) {
+  //   // Clear invalid or corrupt token
+  //   localStorage.removeItem('token');
+  //   return <Navigate to="/login" />; // Redirect to login if token is invalid
+  // }
 };
 
 export default ProtectedRoute;
